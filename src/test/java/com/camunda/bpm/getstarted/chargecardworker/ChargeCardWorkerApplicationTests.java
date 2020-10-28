6dea598197e655db86b29a6459309899bb448a0e
muhammadakbaar <mohammadakbaar11@gmail.com>
2020-10-28 14:38:04 +0700

    hello world with camunda bpm

---
+++ src/test/java/com/camunda/bpm/getstarted/chargecardworker/ChargeCardWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camunda.bpm.getstarted.chargecardworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargeCardWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
